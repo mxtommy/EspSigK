c873ef82d75fef956a6719d8b3aee81cdcb9eb97
Thomas St.Pierre <thomas@samoht.ca>
2018-06-21 21:28:50 -0400

    let client loop run

--- EspSigK.cpp
+++ EspSigK.cpp
@@ -334,7 +334,7 @@ void EspSigK::connectWebSocketClient() {
   }
 
   webSocketClient.begin(host, port, "/signalk/v1/stream?subscribe=none");
-  //wsClientConnected = true;
+  wsClientConnected = true;
 }
 
 void webSocketClientEvent(WStype_t type, uint8_t * payload, size_t length) {
